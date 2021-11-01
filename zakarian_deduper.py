1163147c4864c63d2451f0ba68e46f8f82d27ba1
Christina Zakarian <czakarian@github.com>
2021-11-01 16:01:56 -0700

    finished version of zakarian_deduper.py and Bioinfo.py

--- zakarian_deduper.py
+++ zakarian_deduper.py
@@ -1,48 +1,18 @@
 #!/usr/bin/env python
 
-""" This program performs deduplicating of PCR duplicates from RNA-seq data. For the script to work, 
-need to provide a sorted SAM file as input. Random UMIs or known UMIs. Single or Paired end. """
+""" This program performs deduplicating/removal of PCR duplicates from RNA-seq data. For the script to work, 
+need to provide a sorted SAM file of uniquely mapped reads as input. 
+Random UMIs or known UMIs. Single or Paired end. """
 
 import argparse
-#import Bioinfo
-import re 
-
-def get_strand(flag:int) -> bool :
-    """This function returns the strand for a read in a SAM file based on the bit flag value. 
-    Returns True if positive strand and False if reverse strand."""
-    return (flag & 16) != 16
-
-def get_start(start_pos:int, cigar:str, strand:bool) -> int:
-    """This function returns the corrected start position (adjusted for soft clipping) and the actual start position for reads mapping to reverse strand."""
-    adjust = 0
-    if strand:
-        result = re.match('^([0-9]+)S', cigar)
-        if result:  
-            adjust = 0 - int(result[1])
-    else:
-        # for - strand, sum M/D/N/S values and add that to start_pos, ignore inserts and soft clipping at the beginning   
-        result_S = re.search('([0-9]+)S$', cigar)
-        result_M = re.findall('([0-9]+)M', cigar)
-        result_D = re.findall('([0-9]+)D', cigar)
-        result_N = re.findall('([0-9]+)N', cigar)
-        if result_S:
-            adjust += int(result_S[1])
-        if result_M:
-            adjust += sum(map(int, result_M))
-        if result_D:
-            adjust += sum(map(int, result_D))
-        if result_N:
-            adjust += sum(map(int, result_N))
-        # subtract 1 to correct for position 
-        adjust -= 1
-    return start_pos + adjust
+import Bioinfo 
 
 def get_args():
     """This function returns the parser arguments entered in command line"""
-    parser = argparse.ArgumentParser(description="A program to remove PCR duplicates from SAM file")
-    parser.add_argument("-f", "--file", help="Absolute file path of SAM file", required=True)
-    parser.add_argument("-p", "--paired", help="Designates file is paired end", action='store_true')
-    parser.add_argument("-u", "--umi", help="Designates file containing list of UMIs, unset if randomers used")
+    parser = argparse.ArgumentParser(description="This program removes PCR duplicates from a sorted SAM file of uniquely mapped reads.")
+    parser.add_argument("-f", "--file", help="File path of SAM file", required=True)
+    parser.add_argument("-p", "--paired", help="Designates a paired end SAM file", action='store_true')
+    parser.add_argument("-u", "--umi", help="Designates file containing list of known UMIs, if not provided will assume random UMIs")
     return parser.parse_args()
 
 # store the command line args in variables
@@ -52,62 +22,54 @@ paired = args.paired
 umi_file = args.umi
 
 if args.paired:
-    exit("Warning: This script does not yet handle paired end files. Exiting.\n")
+    exit("Warning: This script does not yet handle paired end files. Exiting.")
+if not args.umi:
+    exit("Warning: This script does not yet handle ramdom UMIs. Please provide list of UMIs in a text file. Exiting.")
 
 # Initialize variables 
-UMI_pos_dict = {} # Dictionary to store encountered positions under each UMI (Key = UMI, Value = Set of tuples storing start position and strand) // will be reset for each chromosome
+UMI_pos_dict = {} # Dictionary to store UMIs and encountered positions (Key = UMI, Value = Set of tuples of form: (start,strand) // dont need to store chromosome because dictionary will be reset for each chromosome
 invalid_UMIs = 0 # counter to keep track of the total number of invalid UMIs encountered
 dups = 0 # counter to keep track of how many PCR duplicates we encounter 
-chrom = "1" # will keep track of which chromosome we are currently on while parsing so we don't have to store chrom individually for each read 
+chrom = "1" # track which chromosome is being parsing so we don't have to store chrom individually for each read in our dictionary
+unique_reads = {"1":0} # tracks # of unique reads per chromosome
 
-count = 0
-y = 0
-
-# Store list of given UMIs in a set (if given) and initialize as keys in our dictionary
-if umi_file:
-    with open(umi_file, "r") as fr:
-        for line in fr:
-            line = line.strip()
-            UMI_pos_dict[line] = set() # set value as an empty set
-  
-
-# changes for random UMIs?
-# will add to dict if no N's, so add to the else part 
-
-
-with open(file, "r") as fr, open("out.sam", "w") as fw:
+# Initialize list of UMIs as keys in our dictionary
+with open(umi_file, "r") as fr:
     for line in fr:
-        # if we are at header line, write line to output file
+        UMI_pos_dict[line.strip()] = set() # set value as an empty set
+
+with open(file, "r") as fr, open(file[:-4] + "_deduped.sam", "w") as fw:
+    for line in fr:
+        # write header lines to output file
         if line[0] == "@":
             fw.write(line)
         else:
             cols = line.split()
             # extract and store the umi 
             umi = cols[0].split(":")[-1] 
-            # if umi in given umi list proceed, otherwise skip read
+            # if umi is valid proceed, otherwise skip read
             if umi in UMI_pos_dict:
                 # if we reach next chromosome, clear out the dictionary before proceeding
                 if cols[2] != chrom:
-                    print(chrom, " ", count)
                     chrom = cols[2]
-                    count = 0
                     UMI_pos_dict = {k: set() for k in UMI_pos_dict.keys()} # will reset values of all umi keys to empty set 
-                # extract and store the starting postion and strand 
-                strand = get_strand(int(cols[1]))
-                start_pos = get_start(int(cols[3]), cols[5], strand)
+                    unique_reads[chrom] = 0
+                # extract and store the corrected start postion and strand 
+                strand = Bioinfo.get_strand(int(cols[1]))
+                start_pos = Bioinfo.get_start(int(cols[3]), cols[5], strand)
                 # if we haven't already encountered this pos/strand with this umi, add to set for that umi and write read to output file 
                 if (start_pos, strand) not in UMI_pos_dict[umi]:
                     UMI_pos_dict[umi].add((start_pos, strand))
                     fw.write(line)
-                    count += 1
+                    unique_reads[chrom] += 1
                 else:
-                    if chrom == "Y":
-                        y += 1
                     dups += 1
             else:
                 invalid_UMIs += 1
 
-print(chrom, " ", count)
+# Output summary statistics
 print("Duplicates Removed: ", dups)
 print("Invalid UMIs: ", invalid_UMIs)
-print("Y", y)
+print("Unique Reads Per Chromosome:")
+for c in unique_reads:
+    print(c, "\t", unique_reads[c])
